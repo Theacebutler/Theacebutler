b80afe5952a85fcf91da5cdc97e9b274d9111c5a
Theacebutler <Theacebutler@gmail.com>
2025-09-30 00:47:33 -0400

    delete django dirs

--- my-site/MySite/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views as v
-
-urlpatterns = [
-    path('', v.HomeView.as_view() ,name='home')
-]
--- vite-project/src/copmonents/Sites.jsx
+++ vite-project/src/copmonents/Sites.jsx
@@ -13,9 +13,9 @@ async function get_image_for_apod() {
 const data = await get_image_for_apod()
 
 export default function Sites(props) {
-  return (
-    <div className='grid md:grid-cols-2 h-2/5'>
-        <SiteCard title={'NASA Image of the day'} url={'https://nasa-image-of-te-day.onrender.com'} description={'get a new image evry day'} previewUrl={data.url}/>
-    </div>
-  )
+    return (
+        <div className='grid md:grid-cols-2 h-2/5'>
+            <SiteCard title={'NASA Image of the day'} url={'https://nasa-image-of-te-day.onrender.com'} description={'get a new image evry day'} previewUrl={data?.url} />
+        </div>
+    )
 }
